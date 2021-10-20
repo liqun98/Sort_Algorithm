c6fe5d1e30abdbf9f63843e21ec1962af6b909a1
CodeMaker-Zhao <zlq1440169549@qq.com>
2021-10-20 21:16:09 +0800

    delete lc

--- main.cpp
+++ main.cpp
@@ -49,28 +49,8 @@ bool test(int times, void (*pf)(vector<int>&)) {
     return res;
 }
 
-int lc(vector<int> nums, int target) {
-    const int sum = accumulate(nums.begin(),nums.end(),0);
-    vector<bool> can(sum+1, false);
-    can[0] = true;
-    for (auto num: nums) {
-        can[num] = true;
-    }
-    for (auto num: nums) {
-        for (int i = sum;i >= num;i--) {
-            can[i] = can[i] || can[i-num];
-        }
-    }
-    for (int i = target;i <= sum;i++) {
-        if (can[i]) {
-            return i;
-        }
-    }
-    return 0;
-}
-
 
 int main() {
-    cout << lc(vector<int>{25,5,80,30,120},100);
+    cout << (test(10, quickSort) ? "valid" : "invalid") << endl;
     return EXIT_SUCCESS;
 }
